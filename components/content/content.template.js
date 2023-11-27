5024bc4025455be4bb1fe8ac08df7c6f786c02d5
parakhsh <parakhsh.dowlatshahy@gmail.com>
2023-11-27 13:13:40 +0100

    components folder created pages folder created and html moved to components

---
+++ components/content/content.template.js
@@ -0,0 +1,14 @@
+
+ export default function content(){
+
+return
+    <div>
+<h1>welcome Mirjam</h1>
+
+
+<div>
+  <h1>HiCoders 23/24 LMS</h1>
+  <p>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+</div>
+</div>
+}
---
+++ components/footer/footer-template.js
@@ -0,0 +1,12 @@
+
+export default function footer (){
+    return
+     <footer class="footer">
+    <div>
+      <ul>
+        <li><a href="">About us</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+    </footer>
+}
